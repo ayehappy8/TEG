7078d62b6d81f3b78bb18e5511a248421a17a821
ayehappy8 <sebaaraus@gmail.com>
2023-12-10 17:53:50 -0300

    tarea

---
+++ output/Tarea5/Tarea5.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main() {
+    //Ingreso de valores
+    int c_procesos, c_bloques, i;
+    printf("Ingrese el número de procesos:");
+    scanf("%d", &c_procesos);
+
+    int procesos[c_procesos];
+    printf("\nIngrese el tamaño de los procesos:\n");
+    for (i = 0; i < c_procesos; i++) {
+        scanf("%d", &procesos[i]);
+    }
+    printf("Ingrese el número de bloques de memoria:");
+    scanf("%d", &c_bloques);
+
+    int bloques[c_bloques];
+    printf("\nIngrese el tamaño de los bloques de memoria:\n");
+    
+    int total = 0;
+    for (i = 0; i < c_bloques; i++) {
+        scanf("%d", &bloques[i]);
+        total = total + bloques[i];
+    }
+    //Ingreso de valores
+    //Inicialización de variables
+    int process1[c_bloques];
+    int job[c_bloques];
+    int fragmentacion[c_bloques];
+    int asignacion[c_bloques];
+
+    for (i = 0; i < c_bloques; i++) {
+        asignacion[i] = 0;
+    }
+    //Inicialización de variables
+    //Algoritmo
+    int j, memoria_usada = 0;
+    i = 0;
+
+    while (i < c_procesos) {
+        int max = -1, indice = -1, k = -1, max1;
+        for (j = 0; j < c_bloques; j++) {
+            max1 = bloques[j];
+        //Donde se encuentra el bloque de memoria mas grande donde se pueda acomodar el procesos actual
+            if (max1 >= max && asignacion[j] == 0 && max1 >= procesos[i]) {
+                max = max1;
+                indice = j;
+            } else {
+                if (asignacion[j] == 0) {
+                    process1[j] = 0;
+                    job[j] = 0;
+                    fragmentacion[j] = bloques[j];
+                }
+            }
+        }
+        if (k != indice) {
+            process1[indice] = procesos[i];
+            job[indice] = i + 1;
+            fragmentacion[indice] = bloques[indice] - procesos[i];
+            memoria_usada = memoria_usada + procesos[i];
+            asignacion[indice] = 1;
+            int l;
+        }
+        i++;
+    }
+    //Algoritmo
+    //Impresión de datos
+    printf("Tamaño del bloque\tTamaño del proceso\tNúmero de proceso\tfragmentación\n");
+    for (i = 0; i < c_bloques; i++) {
+        printf("%d\t\t\t%d\t\t\t%d\t\t\t%d\n", bloques[i], process1[i], job[i], fragmentacion[i]);
+    }
+
+    printf("Total de asignación de memoria:%d\n", total);
+    printf("Memoria utilizada:%d\n", memoria_usada);
+    //Impresión de datos
+    return 0;
+}
